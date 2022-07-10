71858c73ae44a854839404c778f5ef7c083dd17f
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-07-10 15:49:51 -0400

    Some fixes and major release

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::io::{Read, Result, stdin};
+use std::io::{ Read, Result, stdin };
 use std::process::exit;
 
 const NAME: &str = "xorsum";
-const VERSION: &str = "0.0.1";
+const VERSION: &str = "1.0.0";
 
 const HELP_CMD: [&str; 2] = ["-h", "--help"];
 const VER_CMD: [&str; 2] = ["-v", "--version"];
+const BRIEF_CMD: [&str; 2] = ["-b", "--brief"];
+const LOWER_CMD: [&str; 2] = ["-c", "--lower"];
+const UPPER_CMD: [&str; 2] = ["-C", "--UPPER"];
+
 
 fn main() -> Result<()> {
 	let args: Vec<String> = std::env::args().collect();
@@ -21,11 +25,11 @@ fn main() -> Result<()> {
 		if is_len {
 			byte_len = arg.parse().unwrap();
 			is_len = false;
-			continue;
+			continue
 		}
 		if arg == "--len" || arg == "-l" {
 			is_len = true;
-			continue;
+			continue
 		}
 
 		if arg == HELP_CMD[0] || arg == HELP_CMD[1] {
@@ -34,33 +38,32 @@ fn main() -> Result<()> {
 			println!("OPTIONS:");
 			println!("{}, {}		Print help", HELP_CMD[0], HELP_CMD[1]);
 			println!("{}, {}		Print version number", VER_CMD[0], VER_CMD[1]);
+			println!("{}, {}		Only hash, no filename", BRIEF_CMD[0], BRIEF_CMD[1]);
+			println!("{}, {}		lowercase hex (default)", LOWER_CMD[0], LOWER_CMD[1]);
+			println!("{}, {}		UPPERcase hex", UPPER_CMD[0], UPPER_CMD[1]);
 			exit(0);
 		}
-		if arg == "--version" || arg == "-v" {
+		if arg == VER_CMD[0] || arg == VER_CMD[1] {
 			println!("{} {}", NAME, VERSION);
 			exit(0);
 		}
 
-		if arg == "--brief" || arg == "-b" {
-			brief = true
-		}
-		if arg == "--upper" {
-			upper = true
-		}
+		if arg == BRIEF_CMD[0] || arg == BRIEF_CMD[1] {brief = true; continue}
+		if arg == UPPER_CMD[0] || arg == UPPER_CMD[1] {upper = true; continue}
+		if arg == LOWER_CMD[0] || arg == LOWER_CMD[1] {upper = false; continue}
 
 		if arg.starts_with("-") {
 			println!("Unrecognized option. Run `{} --help` for details", NAME);
 			exit(1);
 		}
 		else {
-			files.push(arg.to_string())
+			files.push(arg.to_string());
 		}
 	}
 
 	let read_in = files.len() == 0;
 
-	let mut sbox: Vec<u8> = Vec::new(); //state box, IV = 0
-	while sbox.len() < byte_len { sbox.push(0) }
+	let mut sbox: Vec<u8> = vec![0; byte_len]; //state box, IV = 0
 
 	let mut i: usize = 0;
 	if read_in {
@@ -87,6 +90,9 @@ fn main() -> Result<()> {
 			} else {
 				if upper {println!("{:02X?} {}", sbox, path)} else {println!("{:02x?} {}", sbox, path)}
 			}
+
+			sbox.fill(0);
+			assert_eq!(sbox, vec![0; byte_len]);
 		}
 	}
 
