d06e995eed17f07379e66946b481c64f837ee298
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-07-11 02:22:11 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,31 @@ fn bytevec_tohex(vector: &Vec<u8>, upper: bool) -> String {
 	hex
 }
 
+const NAME: &str = "xorsum";
+const VERSION: &str = "2.0.0"; //should be the same as in Cargo.toml
+
+const HELP_ARG: [&str; 2] = ["-h", "--help"];
+const VER_ARG: [&str; 2] = ["-v", "--version"];
+const LEN_ARG: [&str; 2] = ["-l", "--len"];
+const BRIEF_ARG: [&str; 2] = ["-b", "--brief"];
+const RAW_ARG: [&str; 2] = ["-r", "--raw"];
+const LOWER_ARG: [&str; 2] = ["-a", "--lower"];
+const UPPER_ARG: [&str; 2] = ["-A", "--UPPER"];
+
+fn print_help(){
+	println!("Usage: {NAME} [OPTION]... [FILE]...");
+	println!("If no FILES are given, or if FILE is \"-\", reads Standard Input");
+	println!("OPTIONS:");
+	println!("{}, {}		Print help", HELP_ARG[0], HELP_ARG[1]);
+	println!("{}, {}		Print version number", VER_ARG[0], VER_ARG[1]);
+	println!("{}, {}		Digest size in bytes. Default is 16B (128bit)", LEN_ARG[0], LEN_ARG[1]);
+	println!("{}, {}		Only hash, no filename", BRIEF_ARG[0], BRIEF_ARG[1]);
+	println!("{}, {}		Print raw bytes, no hex. `{}` is implied. Ignores all files, but the 1st", RAW_ARG[0], RAW_ARG[1], BRIEF_ARG[0]);
+	println!("{}, {}		lowercase hex (default)", LOWER_ARG[0], LOWER_ARG[1]);
+	println!("{}, {}		UPPERCASE hex", UPPER_ARG[0], UPPER_ARG[1]);
+}
+
 fn main() -> std::io::Result<()> {
-	const NAME: &str = "xorsum";
-	const VERSION: &str = "2.0.0"; //should be the same as in Cargo.toml
-
-	const HELP_CMD: [&str; 2] = ["-h", "--help"];
-	const VER_CMD: [&str; 2] = ["-v", "--version"];
-	const BRIEF_CMD: [&str; 2] = ["-b", "--brief"];
-	const LOWER_CMD: [&str; 2] = ["-a", "--lower"];
-	const UPPER_CMD: [&str; 2] = ["-A", "--UPPER"];
-	const LEN_CMD: [&str; 2] = ["-l", "--len"];
-	const RAW_CMD: [&str; 2] = ["-r", "--raw"];
-	/*
-	`--raw` Writes raw output bytes to stdout, rather than hex.
-	--no-names is implied. In this case, only a single input is allowed.
-	*/
-
 	let mut paths: Vec<String> = Vec::new();
 	let mut brief = false;
 	let mut upper = false; //uppercase hex
@@ -46,40 +55,31 @@ fn main() -> std::io::Result<()> {
 			is_len = false;
 			continue
 		}
-		if arg == LEN_CMD[0] || arg == LEN_CMD[1] {
+		if arg == LEN_ARG[0] || arg == LEN_ARG[1] {
 			is_len = true;
 			continue
 		}
 
-		if arg == HELP_CMD[0] || arg == HELP_CMD[1] {
-			println!("Usage: {NAME} [OPTION]... [FILE]...");
-			println!("If no FILES are given, or if FILE is \"-\", reads Standard Input");
-			println!("OPTIONS:");
-			println!("{}, {}		Print help", HELP_CMD[0], HELP_CMD[1]);
-			println!("{}, {}		Print version number", VER_CMD[0], VER_CMD[1]);
-			println!("{}, {}		Only hash, no filename", BRIEF_CMD[0], BRIEF_CMD[1]);
-			println!("{}, {}		lowercase hex (default)", LOWER_CMD[0], LOWER_CMD[1]);
-			println!("{}, {}		UPPERCASE hex", UPPER_CMD[0], UPPER_CMD[1]);
-			println!("{}, {}		Digest size in bytes. Default is 16B (128bit)", LEN_CMD[0], LEN_CMD[1]);
-			println!("{}, {}		No hex, only raw bytes. `{}` is implied. Ignores all files, but the 1st", RAW_CMD[0], RAW_CMD[1], BRIEF_CMD[0]);
+		if arg == HELP_ARG[0] || arg == HELP_ARG[1] {
+			print_help();
 			return Ok(())
 		}
-		if arg == VER_CMD[0] || arg == VER_CMD[1] {
+		if arg == VER_ARG[0] || arg == VER_ARG[1] {
 			println!("{NAME} {VERSION}");
 			return Ok(())
 		}
 
-		if arg == BRIEF_CMD[0] || arg == BRIEF_CMD[1] {brief = true; continue}
-		if arg == RAW_CMD[0] || arg == RAW_CMD[1] {raw = true; continue}
-		if arg == UPPER_CMD[0] || arg == UPPER_CMD[1] {upper = true; continue}
-		if arg == LOWER_CMD[0] || arg == LOWER_CMD[1] {upper = false; continue}
+		if arg == BRIEF_ARG[0] || arg == BRIEF_ARG[1] {brief = true; continue}
+		if arg == RAW_ARG[0] || arg == RAW_ARG[1] {raw = true; continue}
+		if arg == UPPER_ARG[0] || arg == UPPER_ARG[1] {upper = true; continue}
+		if arg == LOWER_ARG[0] || arg == LOWER_ARG[1] {upper = false; continue}
 
 		if arg.starts_with("-") {
 			println!("Unrecognized option. Run `{NAME} --help` for details");
-			return Ok(())
+			return Ok(()) //IDK if this is good practice lol
 		}
 		else {
-			paths.push(arg);
+			paths.push(arg); //interpret as filename
 		}
 	}
 	if raw {brief = true} //avoid bugs
@@ -91,8 +91,7 @@ fn main() -> std::io::Result<()> {
 		if digest_len > 0 {
 			for b in std::io::stdin().bytes() {
 				sbox[i] ^= b.unwrap();
-				i += 1;
-				i %= digest_len;
+				i = (i + 1) % digest_len;
 			}
 		}
 		println!("{}", bytevec_tohex(&sbox, upper))
@@ -104,8 +103,7 @@ fn main() -> std::io::Result<()> {
 				//I hope this uses a buffer to prevent RAM from exploding
 				for b in f.bytes() {
 					sbox[i] ^= b.unwrap();
-					i += 1;
-					i %= digest_len;
+					i = (i + 1) % digest_len;
 				}
 			}
 			if brief { println!("{}", bytevec_tohex(&sbox, upper)) }
