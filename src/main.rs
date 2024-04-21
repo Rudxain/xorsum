951c8c28d1b1486d3f3604a610585981c14dec9b
Ricardo Fernández Serrata <rudxain@localhost.localdomain>
2024-04-21 14:10:25 -0400

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@
 	clippy::empty_structs_with_brackets,
 	clippy::format_push_string
 )]
-//can't `forbid`, blame `clap::Parser`
+// can't `forbid`, blame `clap::Parser`
 #![deny(
 	clippy::mem_forget,
 	clippy::large_include_file,
@@ -33,7 +33,7 @@ mod module;
 #[allow(clippy::wildcard_imports)]
 use crate::module::*;
 
-///crate and program name
+/// crate and program name
 const NAME: &str = "xorsum";
 
 #[derive(Parser)]
@@ -43,15 +43,15 @@ const NAME: &str = "xorsum";
 	long_about = "If no FILES are given, or if FILE is \"-\", reads Standard Input"
 )]
 struct Cli {
-	///Digest size in bytes (prior to hex-encoding)
+	/// Digest size in bytes (prior to hex-encoding)
 	#[clap(short, long, default_value_t = DEFAULT_LEN, value_parser)]
 	length: usize,
 
-	///Only print hash, no filenames
+	/// Only print hash, no filenames
 	#[clap(short, long, action)]
 	brief: bool,
 
-	///Files to hash
+	/// Files to hash
 	#[clap(value_parser)]
 	file: Vec<std::path::PathBuf>,
 }
@@ -65,12 +65,12 @@ fn main() {
 	let cli = Cli::parse();
 
 	let stdin_v = stdin();
-	//to print without `lock`
+	// to print without `lock`
 	let mut stdout_v = stdout();
 	let mut stderr_v = stderr();
 
-	//allocate once, reuse everywhere (remember to reset!)
-	let mut sbox = vec![0; cli.length]; //state box, IV = 0
+	// allocate once, reuse everywhere (remember to reset!)
+	let mut sbox = vec![0; cli.length]; // state box, IV = 0
 
 	if cli.file.is_empty() {
 		sbox = stream_processor(stdin_v, sbox).unwrap();
@@ -84,7 +84,7 @@ fn main() {
 	} else {
 		for path in cli.file {
 			if path == Path::new("-") {
-				//avoid creating multiple BRs on the same stdin (just in case)
+				// avoid creating multiple BRs on the same stdin (just in case)
 				sbox = stream_processor(stdin(), sbox).unwrap();
 			} else {
 				match std::fs::File::open(&path) {
@@ -98,7 +98,7 @@ fn main() {
 
 			let hex = u8vec_to_hex_outplace(&sbox);
 			if cli.brief {
-				writeln!(stdout_v, "{}", hex).unwrap();
+				writeln!(stdout_v, "{hex}").unwrap();
 			} else {
 				writeln!(stdout_v, "{} {}", hex, path.display()).unwrap();
 			}
