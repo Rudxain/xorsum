b83233b813a1765652a80a82dbe17b08704b6fba
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-04-23 02:46:11 -0400

    mv hasher into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,52 @@
 #![cfg_attr(not(test), no_std)]
-pub mod utils;
-pub use crate::utils::hasher;
+mod utils;
+#[allow(clippy::wildcard_imports)]
+use crate::utils::*;
+
+/// digests `inp` into `sbox` in-place.
+pub fn hasher<'a, T>(inp: &'a [T], sbox: &mut [T])
+where
+	T: core::ops::BitXorAssign<&'a T>,
+{
+	let len = sbox.len();
+	if len == 0 {
+		return;
+	};
+	// faster than `% len` indexing, because of data-parallelism (and avoids div).
+	// however, if `len` is too big, `chunk` will be allowed to be big too.
+	for chunk in inp.chunks(len) {
+		// this is correct,
+		// because the last chunk doesn't need to be isometric
+		chunk.iter().zip(&mut *sbox).for_each(|(i, s)| *s ^= i);
+	}
+}
+
+/// digests `inp` into `sbox` in-place.
+pub fn hasher_alt<'a, T>(inp: &'a [T], sbox: &mut [T])
+where
+	T: core::ops::BitXorAssign<&'a T>,
+{
+	if sbox.is_empty() {
+		return;
+	};
+	let mut i: usize = 0;
+	// do we really need chunked iter?
+	for chunk in inp.chunks(DEFAULT_BUF_LEN) {
+		for b in chunk {
+			sbox[i] ^= b;
+
+			// rustc should easily optimize this
+			//i = (i + 1) % sbox.len()
+			i += 1;
+			if i >= sbox.len() {
+				i = 0;
+			};
+		}
+	}
+}
 
 pub struct Hasher;
-
-impl Hasher {
+pub impl Hasher {
 	// for streaming support.
 	fn update(&self) {}
 	// wouldn't an iterator be more elegant?
